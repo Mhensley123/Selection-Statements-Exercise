8d927f888c54ee5739b9a2da6e16e0556fc687aa
Maria Hensley <mscelie34@zoho.com>
2022-11-07 19:36:22 -0600

    finished selection statements1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to my favorite number guessing game!");
+            Console.WriteLine("Try to guess my favorite number!");
+            Console.WriteLine("What is the upper limit for the range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse); 
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high! Guess again.");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low. Guess again");
+            }
+            else
+            {
+                Console.WriteLine("You are on point!");
+            }
         }
     }
 }
